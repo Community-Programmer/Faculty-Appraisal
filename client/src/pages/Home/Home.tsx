5e2a44c219201552edb273e4caac336cc83448cd
herezTamanna <iamtam.3003@gmail.com>
2024-09-06 21:11:49 +0530

    edited homepage

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,80 +1,100 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './styles.css';
 import cardBg1 from './cardBg1.jpg'
-import admin from './images.png'
+import admin from './admin.png'
 import facul from './Teachers-icon.png'
+import { Button } from "@/components/ui/button"
+
 export default function Home() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const offset = window.scrollY;
+      if (offset > 325) { 
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
 
   return (
       <div>
-        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-          <div style={{ textShadow: '3px 3px 3px gray', fontSize: '40px' }}>
-            <b>GuruVista</b>
-          </div>
-        </div>
-        <div>
-          <p className="wave-text">
-            <span style={{ textShadow: '2px 2px 2px red',margin:'10px'}}>An Automated System for Career Advancements of the Faculties</span>
-          </p>
-        </div>
-        <div className="info" style={{ display: 'flex' }}>
-          <div style={{ width: '100%' }}>
-            <p style={{ textAlign: 'center', fontWeight: 'bolder', textShadow: '1px 1px 1px red' }}>DIVERSITY DRIVES INNOVATION</p>
-            <p style={{ marginLeft: '20%' }}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae dolor, veniam neque quas error numquam fugiat laudantium optio odio omnis incidunt dolorum vel consectetur itaque? Est minus voluptas, unde quasi modi nulla illo rerum? Atque quasi nihil dolore porro, doloribus necessitatibus minus omnis. Odit doloribus accusamus labore magni repellat iusto tempore qui corrupti reprehenderit accusantium, sint illo dolorum earum incidunt deleniti itaque vitae? Tempora inventore eius corporis eligendi cumque.</p>
-          </div>
-          <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
-            <div style={{ display: 'flex', justifyContent: 'space-between', width: '80%', height: '80%' }}>
-              {/* Top Quadrant */}
-              <div style={{ flex: 1, border: '1px solid #ccc', padding: '10px', margin: '5px', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
-                <p style={{ fontFamily: 'cursive', fontSize: '20px' }}><u>Publications</u></p>
-                <p>Tracking the publications made by faculty members and their contribution towards innovation beyond the classroom.</p>
-              </div>
+        <div className="flex justify-between">
+  <div className="text-black text-shadow-lg text-4xl">
+    <b>GuruVista</b>
+  </div>
+</div>
+<div>
+  <p className="wave-text">
+    <span className="head text-black m-3">An Automated System for Career Advancements of the Faculties</span>
+  </p>
+</div>
+<div className="info bg-black text-white flex">
+  <div className="w-full">
+    <p className=" head2 text-center font-bold m-2">DIVERSITY DRIVES INNOVATION</p>
+    <p className="ml-20">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae dolor, veniam neque quas error numquam fugiat laudantium optio odio omnis incidunt dolorum vel consectetur itaque? Est minus voluptas, unde quasi modi nulla illo rerum? Atque quasi nihil dolore porro, doloribus necessitatibus minus omnis. Odit doloribus accusamus labore magni repellat iusto tempore qui corrupti reprehenderit accusantium, sint illo dolorum earum incidunt deleniti itaque vitae? Tempora inventore eius corporis eligendi cumque.</p>
+  </div>
+  <div className="flex flex-col items-center justify-center h-screen">
+    <div className="flex justify-between w-4/5 h-4/5">
+      {/* Top Quadrant */}
+      <div className="flex-1 border border-gray-300 p-2 m-1 flex flex-col justify-center items-center">
+        <p className="font-cursive text-xl"><u>Publications</u></p>
+        <p>Tracking the publications made by faculty members and their contribution towards innovation beyond the classroom.</p>
+      </div>
 
-              {/* Right Quadrant */}
-              <div style={{ flex: 1, border: '1px solid #ccc', padding: '10px', margin: '5px', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
-                <p style={{ fontFamily: 'cursive', fontSize: '20px' }}><u>Event Participation</u></p>
-                <p>Tracking the events where the faculties have participated and the upcoming events in which they can participate as per their expertise.</p>
-              </div>
-            </div>
+      {/* Right Quadrant */}
+      <div className="flex-1 border border-gray-300 p-2 m-1 flex flex-col justify-center items-center">
+        <p className="font-cursive text-xl"><u>Event Participation</u></p>
+        <p>Tracking the events where the faculties have participated and the upcoming events in which they can participate as per their expertise.</p>
+      </div>
+    </div>
 
-            <div style={{ display: 'flex', justifyContent: 'space-between', width: '80%', height: '80%' }}>
-              {/* Left Quadrant */}
-              <div style={{ flex: 1, border: '1px solid #ccc', padding: '10px', margin: '5px', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
-                <p style={{ fontFamily: 'cursive', fontSize: '20px' }}><u>Projects & Lectures</u></p>
-                <p>Tracking the projects made by faculty members and lectures given.</p>
-              </div>
+    <div className="flex justify-between w-4/5 h-4/5">
+      {/* Left Quadrant */}
+      <div className="flex-1 border border-gray-300 p-2 m-1 flex flex-col justify-center items-center">
+        <p className="font-cursive text-xl"><u>Projects & Lectures</u></p>
+        <p>Tracking the projects made by faculty members and lectures given by them.</p>
+      </div>
 
-              {/* Bottom Quadrant */}
-              <div style={{ flex: 1, border: '1px solid #ccc', padding: '10px', margin: '5px', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
-                <p style={{ fontFamily: 'cursive', fontSize: '20px' }}><u>Performance</u></p>
-                <p>Visualizing the performance of faculty members using pie charts and graphs.</p>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div style={{ display: 'flex', justifyContent: 'center', margin: '40px', marginTop: '100px' }}>
-          <div style={{ width: '22%', backgroundColor: 'white', margin: '40px', display: 'flex', flexDirection: 'column', justifyContent: 'space-between', border: '3px solid red', position: 'relative' }}>
-            <img src={cardBg1} alt="Image not Found" style={{ width: '100%' }} />
-            <div style={{ position: 'absolute', left: '50%', top: '-25%', transform: 'translateX(-50%)', display: 'flex', justifyContent: 'center' }}>
-              <img src={admin} alt="Image not Found" style={{ border: '2px solid red', borderRadius: '50%', width: '100%', backgroundColor: 'white' }} />
-            </div>
-            <p style={{ fontWeight: 'bolder', color: 'black', textAlign: 'center', fontSize: '25px' }}>Administrator</p>
-            <button style={{ width: '100%', backgroundColor: 'red', margin: '10px 0 0', padding: '12px', borderRadius: '15px' }}>
-              <a href="/adminLogin" style={{ textDecoration: 'none', color: 'white' }}>Login</a>
-            </button>
-          </div>
+      {/* Bottom Quadrant */}
+      <div className="flex-1 border border-gray-300 p-2 m-1 flex flex-col justify-center items-center">
+        <p className="font-cursive text-xl"><u>Performance</u></p>
+        <p>Visualizing the performance of faculty members using pie charts and graphs.</p>
+      </div>
+    </div>
+  </div>
+</div>
+<div className={`flex justify-center transition-opacity duration-300 ${isVisible ? 'opacity-100' : 'opacity-0'} text-center my-6`}>
+    <p className="text-2xl font-bold bg-red-500 w-1/4"><u>Account Login Here</u></p>
+</div>
+<div className="flex justify-center my-6 mt-24">
+  <div className="w-1/5 bg-white m-1 mr-4 flex flex-col justify-between border-2 border-red-500 relative">
+    <img src={cardBg1} alt="Image not Found" className="w-full" />
+    <div className="absolute left-1/2 top-[-25%] transform -translate-x-1/2 flex justify-center">
+      <img src={admin} alt="Image not Found" className="border-2 border-red-500 rounded-full w-full bg-white" />
+    </div>
+    <p className="font-bold text-black text-center text-2xl">Administrator</p>
+    <Button variant="outline">Login</Button>
+  </div>
 
-          <div style={{ width: '22%', backgroundColor: 'white', margin: '40px', display: 'flex', flexDirection: 'column', justifyContent: 'space-between', border: '3px solid red', position: 'relative' }}>
-            <img src={cardBg1} alt="Image not Found" style={{ width: '100%' }} />
-            <div style={{ position: 'absolute', left: '50%', top: '-25%', transform: 'translateX(-50%)', display: 'flex', justifyContent: 'center' }}>
-              <img src={facul} alt="Image not Found" style={{ border: '2px solid red', borderRadius: '50%', width: '100%', backgroundColor: 'white' }} />
-            </div>
-            <p style={{ fontWeight: 'bolder', color: 'black', textAlign: 'center', fontSize: '25px' }}>Faculty</p>
-            <button style={{ width: '100%', backgroundColor: 'red', margin: '10px 0 0', padding: '12px', borderRadius: '15px' }}>
-              <a href="#" style={{ textDecoration: 'none', color: 'white' }}>Login</a>
-            </button>
-          </div>
-        </div>
+  <div className="w-1/5 bg-white m-1 ml-4 flex flex-col justify-between border-2 border-red-500 relative">
+    <img src={cardBg1} alt="Image not Found" className="w-full" />
+    <div className="absolute left-1/2 top-[-25%] transform -translate-x-1/2 flex justify-center">
+      <img src={facul} alt="Image not Found" className="border-2 border-red-500 rounded-full w-full bg-white" />
+    </div>
+    <p className="font-bold text-black text-center text-2xl">Faculty</p>
+    <Button variant="outline">Login</Button>
+  </div>
+</div>
+
       </div>
   );
 }
